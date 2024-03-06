eb041687f7ff0140af7086dcc1a34b1a4472bb21
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2024-03-06 10:20:07 -0500

    add payload example

--- custom_extractor.js
+++ custom_extractor.js
@@ -1,5 +1,18 @@
+// This function is used to extract the traceId, parentId, and sampleMode from the incoming event.
+// An example json payload is:
+// {
+//   "request":{
+//     "headers":{
+//         "traceparent":"00-0000000000000000abcdef1234567890-12341e2abcdefabc-02",
+//         "x-datadog-parent-id": "1234567123456789000",
+//         "x-datadog-trace-id": "9876543217654321000",
+//         "x-datadog-sampling-priority": 1,
+//         "x-datadog-origin": "rum"
+//     }}
+// }
+
 exports.nestedJsonExtractor = (event, _) => {
-  const nestedJson = event.request.headers; // the payload is nested inside the request key
+  const nestedJson = event.request.headers; // the payload is nested inside the "request" key
   const result = {
     traceId: nestedJson["x-datadog-trace-id"],
     parentId: nestedJson["x-datadog-parent-id"],
